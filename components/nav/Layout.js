92437b5391994c15eeab45be0bb55bc447a103b2
Gaetan basset <gbasset@protonmail.com>
2022-08-10 19:57:26 +0200

    reset

---
+++ components/nav/Layout.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react'
+import NavContainer from './NavContainer';
+import classes from './Layout.module.css';
+import Footer from '../Footer/Footer'
+export default function ({ children }) {
+    const [smallS, setSmallS] = useState(false);
+
+    useEffect(() => {
+        const mediaQuery = window.matchMedia("(max-width: 900px)");
+        // addlistener c'est comme addeventlisterner pour les medias queries en JS
+        mediaQuery.addListener(handleMediaQueryChange);
+        handleMediaQueryChange(mediaQuery);
+
+        return () => {
+            mediaQuery.removeListener(handleMediaQueryChange);
+        }
+
+    })
+
+    const handleMediaQueryChange = (mediaQuery) => {
+        if (mediaQuery.matches) {
+            setSmallS(true);
+        } else {
+            setSmallS(false);
+        }
+    }
+    return (
+        <div className={!smallS ? classes.container : classes.container + classes.small}>
+            <nav> <NavContainer /></nav>
+            <main>{children}</main>
+            {smallS &&
+                <Footer />
+            }
+        </div >
+    )
+}
