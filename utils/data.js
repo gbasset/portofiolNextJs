c0f457761957ca26fbf14574a79514cdc276ecb8
Gaetan basset <gbasset@protonmail.com>
2022-12-19 13:17:04 +0100

    wip

--- utils/data.js
+++ utils/data.js
@@ -1,2 +1,3 @@
-export const apiProjects = 'https://enigmatic-taiga-55729.herokuapp.com/'
+// export const apiProjects = 'https://enigmatic-taiga-55729.herokuapp.com/'
+export const apiProjects = 'https://node-api-wyau.onrender.com/'
 // export const apiProjects = 'http://localhost:5000/'
